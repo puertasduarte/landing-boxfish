aec2bec83feccae731db3673d41e490bb6c95a9b
Jesus Puertas <puertasduarte@gmail.com>
2021-06-03 18:34:14 +0200

    toogle-component finished

--- src/app/components/toogle/toogle.component.ts
+++ src/app/components/toogle/toogle.component.ts
@@ -6,14 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toogle.component.scss']
 })
 export class ToogleComponent implements OnInit {
+  
+  toogleMessage = 'Switch to dark mode!';
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
 
-  changeTheme() {
-    console.log('click!');
+  changeTheme(): void {
+    let checkColor= getComputedStyle(document.documentElement).getPropertyValue('--appBackgroundColor');
+
+    if (checkColor.includes('#FFFFFF')) {
+      document.documentElement.style.setProperty('--appBackgroundColor', '#333333');
+      document.documentElement.style.setProperty('--headerBorderColor', '#000000');
+      document.documentElement.style.setProperty('--titleFontColor', '#FFFFFF');
+      document.documentElement.style.setProperty('--subtitleFontColor', '#E5E5E5');
+      document.documentElement.style.setProperty('--toogleFontColor', '#333333');
+      this.toogleMessage = 'Switch to light mode!';
+    } else {
+      document.documentElement.style.setProperty('--appBackgroundColor', '#FFFFFF');
+      document.documentElement.style.setProperty('--headerBorderColor', '#E5E5E5');
+      document.documentElement.style.setProperty('--titleFontColor', '#333333');
+      document.documentElement.style.setProperty('--subtitleFontColor', '#999999');
+      document.documentElement.style.setProperty('--toogleFontColor', '#999999');
+      this.toogleMessage = 'Switch to dark mode!';
+    }
   }
 
 }
